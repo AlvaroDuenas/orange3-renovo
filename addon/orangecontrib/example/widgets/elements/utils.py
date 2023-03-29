92bdabd93e1a5a8a002ebfb3738572765cab4d42
Alvaro Dueñas <alvaro25duenas@gmail.com>
2023-03-29 14:29:20 +0200

    Create utils.py

---
+++ addon/orangecontrib/example/widgets/elements/utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+def sum_spectrum(mz_array, i_array):
+    ''' calculates the sum of all spectra accross the matrix. '''
+    
+    total_spectra = dict()
+    for i in range(len(mz_array)):        # loop over pixels
+        for j in range(len(mz_array[i])): # loop over mz values
+            if mz_array[i][j] == 0 and j != 0:
+                break
+            if mz_array[i][j] in total_spectra.keys():
+                total_spectra[mz_array[i][j]] += i_array[i][j]
+            else:
+                total_spectra[mz_array[i][j]] = i_array[i][j]
+                
+    mz_values = list(total_spectra.keys())
+    i_values = list(total_spectra.values())
+    
+    return mz_values, i_values
+  
+def visualize(im_list):
+  #im_list = loadmat("pruebas\data.mat")['data']
+  max_x, min_x = get_min_max(im_list,0)
+
+  max_y, min_y = get_min_max(im_list,1)
+  im_crude = np.zeros((max_x, max_y), dtype=np.float32)
+  for i in im_list:
+      im_crude[int(i[0])-1, int(i[1])-1] = i[3]
+  out = np.zeros(im_crude.shape, np.float32)
+  normalized = cv2.normalize(im_crude, out, 1.0, 0.0, cv2.NORM_MINMAX)
+  cv2.imshow("image",normalized)
+  cv2.waitKey(0) 
+
+def get_min_max(arr, col):
+  return int(np.max(arr[:,[col]])), int(np.min(arr[:,[col]]))
